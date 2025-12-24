bef947c61da4a96e7c9873eabe3dd7a2cabb0505
Thomas Kyaw <thomas.kyaw.69@gmail.com>
2025-12-25 05:14:01 +0800

    initial commit

---
+++ notification-service/src/test/java/com/thomaskyaw/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomaskyaw.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
